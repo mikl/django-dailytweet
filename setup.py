5b8497a3f2e7e96542ae5c37f1ed1505492cba2c
Mikkel Hoegh <mikkel@hoegh.org>
2009-09-25 23:34:47 +0200

    Defined setup and requirement files.

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='dailytweets',
+    version='0.1',
+    description='Simple reusable Django app to post a message each day on Twitter',
+    author='Mikkel Høgh',
+    author_email='mikkel@hoegh.org',
+    url = 'http://github.com/mikl/django-dailytweet',
+    packages=['dailytweet'],
+)
+
+
