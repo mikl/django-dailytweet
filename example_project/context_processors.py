ff38a6812e2bc351f3a0b92f144a589feb750f17
Mikkel Hoegh <mikkel@hoegh.org>
2009-09-26 14:12:45 +0200

    Exposing current site to all templates.

--- example_project/context_processors.py
+++ example_project/context_processors.py
@@ -1,12 +1,15 @@
 from django.conf import settings
+from django.contrib.sites.models import Site
 
 def site_info(request):
     """
     Provide site-specific context variables.
 
-    In this case, we just make our own STATIC_URL available.
+    Expose the STATIC_URL setting and the current site from the sites
+    framework from django.contrib.
     """
     return {
         'STATIC_URL': getattr(settings,'STATIC_URL', ''),
+        'SITE': Site.objects.get_current(),
     }
 
