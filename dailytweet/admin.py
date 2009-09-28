e16f9938cb9414f3de658ab52a20ecd1d10de610
Mikkel Hoegh <mikkel@hoegh.org>
2009-09-28 07:05:57 +0200

    More informative admin pages.

--- dailytweet/admin.py
+++ dailytweet/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from dailytweet.models import DailyTweet
 
 class DailyTweetAdmin(admin.ModelAdmin):
-    list_display = ('message', 'publish_date', 'published_time')
+    date_hierarchy = 'publish_date'
+    list_display = ('message', 'creation_time', 'publish_date', 'published_time')
+    list_filter = ('creation_time', 'publish_date')
+    ordering = ('published_time', 'publish_date', '-creation_time')
 
 admin.site.register(DailyTweet, DailyTweetAdmin)
 
